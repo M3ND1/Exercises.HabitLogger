d8ffba4e5737c3d623accf9cecd86163ddf63374
Mendi <menkarol27@gmail.com>
2023-02-15 16:30:32 +0100

    Update working, creating column working (not dynamically) TODO: validation check

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -1,15 +1,12 @@
-﻿
-using System.ComponentModel.DataAnnotations;
-using System.Configuration;
-using System.Data.Entity.Core.Mapping;
-using System.Data.SQLite;
+﻿using System.Data.SQLite;
+using System.Globalization;
 using System.Reflection;
 
 namespace HabitLogger
 {
     class Program
     {
-        
+
         static string connection_string = "Data Source=HabitDatabase.db;Version=3;";
 
         static void Main(string[] args)
@@ -34,12 +31,12 @@ namespace HabitLogger
         {
             int input;
             Console.Clear();
-            Console.WriteLine("What do you wanna do here");
+            Console.WriteLine("Welcome to Habit Logger, what do You want to do here?");
             Console.WriteLine("-------------------------\n");
             bool closeApp = false;
-            while(closeApp == false)
+            while (closeApp == false)
             {
-                //Console.Clear();
+                Console.WriteLine("Choose Your Activity");
                 Console.WriteLine("0 - Exit App");
                 Console.WriteLine("1 - Insert");
                 Console.WriteLine("2 - See records");
@@ -47,7 +44,7 @@ namespace HabitLogger
                 Console.WriteLine("4 - Update existing record");
 
                 input = Convert.ToInt32(Console.ReadLine());
-                switch(input)
+                switch (input)
                 {
                     case 0:
                         closeApp = true;
@@ -64,6 +61,12 @@ namespace HabitLogger
                     case 4:
                         Update();
                         break;
+                    case 5:
+                        Create();
+                        break;
+                    case 6:
+                        checkEmptyDb();
+                        break;
                     default:
                         Console.WriteLine("Type valid command.");
                         break;
@@ -74,7 +77,7 @@ namespace HabitLogger
         {
             string date = GetDateInput();
             int times = GetNumberInput();
-            using (SQLiteConnection conn = new SQLiteConnection(connection_string)) 
+            using (SQLiteConnection conn = new SQLiteConnection(connection_string))
             {
                 conn.Open();
                 var tableCmd = conn.CreateCommand();
@@ -93,13 +96,13 @@ namespace HabitLogger
                 var tableCmd = conn.CreateCommand();
                 tableCmd.CommandText = $"SELECT * FROM Gym";
 
-                List<Gym> gymList = new(); 
-                
+                List<Gym> gymList = new();
+
                 SQLiteDataReader reader = tableCmd.ExecuteReader();
-                
-                if(reader.HasRows)
+
+                if (reader.HasRows)
                 {
-                    while(reader.Read())
+                    while (reader.Read())
                     {
                         gymList.Add(
                         new Gym
@@ -111,11 +114,11 @@ namespace HabitLogger
                     }
                 }
                 else { Console.WriteLine("No rows in this table"); }
-                
-                
+
+
                 conn.Close();
 
-                foreach(var gym in gymList)
+                foreach (var gym in gymList)
                 {
                     Console.WriteLine($"Person {gym.Id} on day {gym.Data.ToString("dd-MMM-yyyy")} you were {gym.Times} time/s at gym.");
                     //Console.WriteLine($"On day {gym.Data} you were {gym.Times} time/s at gym.");
@@ -133,14 +136,14 @@ namespace HabitLogger
             Get();
             int number_delete = Convert.ToInt32(Console.ReadLine());
             if (number_delete == 0) GetUserInput();
-            using(var conn = new SQLiteConnection(connection_string))
+            using (var conn = new SQLiteConnection(connection_string))
             {
                 conn.Open();
                 var cmdTable = conn.CreateCommand();
                 cmdTable.CommandText = $"DELETE FROM Gym WHERE Id = '{number_delete}'";
-            
+
                 int rowCount = cmdTable.ExecuteNonQuery();
-                if(rowCount == 0)
+                if (rowCount == 0)
                 {
                     Console.WriteLine("There are no rows to delete.");
                     Delete();
@@ -151,12 +154,17 @@ namespace HabitLogger
             Console.Clear();
             GetUserInput();
         }
-
         private static void Update()
         {
-            //Console.Clear();
             //readline which one (id)
-            string id_name = GetId();
+
+            if (checkEmptyDb() == true)
+            {
+                Console.WriteLine("Returning back to main menu...");
+                System.Threading.Thread.Sleep(2000);
+                GetUserInput(); //if Db empty return to menu 
+            }
+            string id_name = GetId(); //check if there are rows in table
             showDataType(id_name);
             string column_name = GetColumnName();
             string data_type = GetDateInput();
@@ -171,6 +179,47 @@ namespace HabitLogger
             }
 
         }
+        private static void Create()
+        {
+            Console.Clear();
+            Console.WriteLine("What will be the name of your habit that you wanna add?");
+            string habit = Console.ReadLine();
+            Console.Clear();
+
+            System.Threading.Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Choose data type of your habit: ");
+            Console.WriteLine("TEXT");
+            Console.WriteLine("INT");
+            Console.WriteLine("NUMERIC");
+            Console.WriteLine("REAL");
+            string data_type = Console.ReadLine().ToUpper();
+            var propertyInfo = typeof(Gym).GetProperty(habit);
+            if (propertyInfo == null)
+            {
+                var propertyType = Type.GetType($"System.{data_type}");
+                propertyInfo = typeof(Gym).GetProperty(habit, propertyType);
+                if (propertyInfo == null)
+                {
+                    propertyInfo = typeof(Gym).GetProperty(habit, propertyType ?? typeof(string));
+                }
+                Console.WriteLine("Habit added succesfully");
+            }
+            else
+            {
+                Console.WriteLine("Habit exists.");
+                Console.WriteLine("Returning to Menu");
+                GetUserInput();
+            }
+            using (var conn = new SQLiteConnection(connection_string))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = $"ALTER TABLE Gym ADD COLUMN {habit} {data_type}";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
         internal static string GetId()
         {
             Get();
@@ -182,9 +231,16 @@ namespace HabitLogger
         }
         internal static string GetDateInput()
         {
-            Console.WriteLine("Insert Date and when you were at gym. Date format ex. 01-January-2000 \t To exit type: 0");
-            string date = Console.ReadLine();
+            Console.Write("Insert Date when you were at Gym.\n Remember! Example of Date: 01-January-2000 To exit type: 0\t");
+            string date = Console.ReadLine(); //validation
             if (date == "0") GetUserInput();
+            DateTime date_time;
+            while(!DateTime.TryParseExact(date, "dd-MMMM-yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date_time))
+            {
+                Console.WriteLine("Provide proper date. Example: 23-MARCH-2000");
+                date = Console.ReadLine();
+                Console.Clear();
+            }
             Console.Clear();
             return date;
         }
@@ -195,7 +251,7 @@ namespace HabitLogger
             PropertyInfo[] prop = gym.GetProperties(); //array of properies in Gym.cs
             foreach (PropertyInfo p in prop)
             {
-                if(p.Name == "Id") continue; //dont change Id
+                if (p.Name == "Id") continue; //dont change Id
                 Console.WriteLine(p.Name);
             }
             Console.WriteLine("=============");
@@ -207,15 +263,23 @@ namespace HabitLogger
         }
         internal static int GetNumberInput()
         {
-            Console.WriteLine("Insert Times, how many times you weere at gym that day. Numbers only");
-            Console.WriteLine("To exit console type 0.");
-            int times = Convert.ToInt32(Console.ReadLine());
-            if(times == 0) GetUserInput();
+            int times=0;
+            bool validInput = false;
+            while(validInput == false)
+            {
+                Console.WriteLine("Insert how many times you were at gym that day. To exit type 0");
+                string input = Console.ReadLine();
+                validInput = int.TryParse(input, out times);
+
+                if (!validInput) Console.WriteLine("Provide number."); //sleep
+                if (input == "0") GetUserInput();
+                Console.Clear();
+            }
             return times;
         }
         internal static void showDataType(string data_type)
         {
-            using(var conn= new SQLiteConnection(connection_string))
+            using (var conn = new SQLiteConnection(connection_string))
             {
                 conn.Open();
                 var tableCmd = conn.CreateCommand();
@@ -225,6 +289,31 @@ namespace HabitLogger
                 conn.Close();
 
             }
+        } // change name of this method later
+        private static bool checkEmptyDb()
+        {
+            bool sol;
+            using(var conn = new SQLiteConnection(connection_string))
+            {
+                conn.Open();
+                var cmd = conn.CreateCommand();
+                cmd.CommandText = $"SELECT COUNT(*) FROM Gym";
+                int is_Empty = Convert.ToInt32(cmd.ExecuteScalar());
+                if (is_Empty == 0)
+                {
+                    Console.WriteLine("The Table is empty.");
+                    sol = true;
+                }
+                else
+                {
+                    Console.WriteLine("The Table is not empty");
+                    sol = false;
+                }
+                System.Threading.Thread.Sleep(2000);
+                conn.Close();
+            }
+            Console.Clear();
+            return sol;
         }
     }
 
